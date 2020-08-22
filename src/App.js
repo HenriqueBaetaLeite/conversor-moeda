ff43f0dc765529251845c227978809a44be36cae
henrique <hbaetaleite@gmail.com>
2020-08-22 00:24:23 -0300

    atualzizando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Conversor from './components/Conversor';
 
 function App() {
--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -3,47 +3,74 @@ import React, { useState } from 'react';
 const axios = require('axios');
 
 const Conversor = () => {
-  const [moeda1, setMoeda1] = useState(0);
-  const [moeda2, setMoeda2] = useState(0);
-  const [valor, setValor] = useState('');
+  const [moeda1, setMoeda1] = useState('USD');
+  const [moeda2, setMoeda2] = useState('BRL');
+  const [valor, setValor] = useState(1);
+  const [valorConvertido, setValorConvertido] = useState(0);
 
+  const URL = `https://api.ratesapi.io/api/latest?base=${moeda1}&symbols=${moeda2}`;
   const getAPI = () => {
-    axios.get(URL).then((resp) => setMoeda2(resp));
+    axios.get(URL).then((resp) => setValorConvertido(resp.data.rates[moeda2]));
   };
-  console.log('valor digitado no input:', valor, moeda1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     getAPI();
   };
 
+  console.log('valor digitado no input', valor);
+
+  console.log('valor convertido * valor: ', valorConvertido * valor);
+
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
+    <form className="container d-flex flex-column" onSubmit={(e) => handleSubmit(e)}>
       <h2>Conversor de moedas</h2>
 
-      <h4>Moeda 1</h4>
-      <select name="" id="" onChange={(e) => setMoeda1(e.target.value)}>
-        <option value="">Escolha</option>
-        <option value="USD">Dolár</option>
-        <option value="EUR">Euro</option>
-        <option value="BRL">Real</option>
-      </select>
+      <div className="dropdown">
+        <label htmlFor="moeda1">Converta</label>
+        <select name="" id="" onChange={(e) => setMoeda1(e.target.value)}>
+          <option value="">Escolha</option>
+          <option value="USD">Dolár - US$</option>
+          <option value="EUR">Euro - &#8364;</option>
+          <option value="BRL">Real - R$</option>
+        </select>
+      </div>
 
-      <label htmlFor="">Valor a ser convertido</label>
-      <input type="number" onChange={(e) => setValor(e.target.value)} />
+      <div className="form-group">
+        <label className="mr-2" htmlFor="inputValue">
+          Valor a ser convertido
+        </label>
+        <input
+          value={valor}
+          type="number"
+          id="inputValue"
+          onChange={(e) => setValor(e.target.value)}
+        />
+      </div>
 
-      <h4>Moeda 2</h4>
-      <select name="" id="">
-        <option value="">Escolha</option>
-        <option value="">Dolár</option>
-        <option value="EUR">Euro</option>
-        <option value="">Real</option>
-      </select>
+      <div className="form-group">
+        <label htmlFor="moeda2">Para</label>
+        <select name="" id="" onChange={(e) => setMoeda2(e.target.value)}>
+          <option value="">Escolha</option>
+          <option value="USD">Dolár - US$</option>
+          <option value="EUR">Euro - &#8364;</option>
+          <option value="BRL">Real - R$</option>
+        </select>
+      </div>
 
-      <button type="submit">Converter</button>
+      <div>
+        <button className="btn btn-primary mt-4" type="submit">
+          Converter
+        </button>
+      </div>
 
       <h3>Valor convertido:</h3>
-      <h3>{moeda2}</h3>
+      <h3>
+        {(valorConvertido * valor).toLocaleString('pt-br', {
+          style: 'currency',
+          currency: `${moeda2}`,
+        })}
+      </h3>
     </form>
   );
 };
